1544aced090d12e5f3e3eefcdefd2a487eecb14f
Ricardo Amores <rickyah@gmail.com>
2019-03-06 00:50:43 +0100

    allow setting the dry-run for rsync with env DOTFILES_DRY_RUN

--- dotfiles-cmd/share/dotf/file_rsync.rb
+++ dotfiles-cmd/share/dotf/file_rsync.rb
@@ -1,6 +1,6 @@
 class FileRsync
-    @@rsync_args ="-av"
-    @@rsync_test_args = "--dry-run"
+    @@default_args ="-av"
+    @@rsync_test = "--dry-run"
     @@rsync_cmd  ="rsync"
 
     def initialize source_path, dest_path
@@ -8,16 +8,14 @@ class FileRsync
         @dest_path = dest_path
     end
 
-    def test_rsync_files
-        puts '[Test Mode]'
-        rsync_files [@@rsync_args, @@rsync_test_args].join(' ')
-    end
-
     def rsync_files args=nil
-        rsync_args = args || @@rsync_args
+        
+        rsync_args = args || @@default_args
+        rsync_args += " " + @@rsync_test if ENV['DOTFILES_DRY_RUN']
         cmd = "#{@@rsync_cmd} #{rsync_args} #{@source_path} #{@dest_path}"
 
         puts "Command: [#{cmd}]"
+        puts "INFO: dry run: no files will be copied" if ENV['DOTFILES_DRY_RUN']
         puts "List of operations to be performed:"
         puts `#{cmd}`
     end
