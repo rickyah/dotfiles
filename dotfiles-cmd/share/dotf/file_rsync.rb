69c75fe8c877e2c0dd70aeb4478fa4e99728756f
Ricardo Amores <rickyah@gmail.com>
2019-10-05 22:51:23 +0200

    Simplify rsync wrapper code
    squash! Simplify rsync wrapper code and add quiet option

--- dotfiles-cmd/share/dotf/file_rsync.rb
+++ dotfiles-cmd/share/dotf/file_rsync.rb
@@ -1,18 +1,28 @@
 class FileRsync
-    @@rsync_cmd  ="rsync"
-    @@default_args ="-av"
-    @@dry_run_arg = " --dry-run"
 
-    def initialize dry_run: false
-        @rsync_args =  @@default_args
-        @rsync_args += @@dry_run_arg if dry_run
+    @quiet = false
+    @dry_run = false
+
+    def initialize dry_run: false, quiet: false
+
+        @dry_run = dry_run
+        @quiet = quiet
+
+        @rsync_args =  "-av" 
+        @rsync_args += " --dry-run" if @dry_run
+        @rsync_args += " -q" if @quiet
     end
 
     def rsync_files source_path, dest_path 
-        cmd = "#{@@rsync_cmd} #{@rsync_args} #{source_path} #{dest_path}"
+        cmd = "rsync #{@rsync_args} #{source_path} #{dest_path}"
 
-        puts "Executing command: [#{cmd}]"
-        puts `#{cmd}`
+        dry_run_warning = ""
+        dry_run_warning = "(DRY RUN)" if @dry_run
+        puts "#{dry_run_warning} #{source_path} --> #{dest_path}"
+        unless @quiet
+            puts "executing command: [#{cmd}]"
+        end
+        `#{cmd}`
     end
 
 end
