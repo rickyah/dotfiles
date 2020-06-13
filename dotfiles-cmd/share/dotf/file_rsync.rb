9081d59e1cbda086fb6fb70c3375102bddd26f2d
Ricardo Amores <rickyah@gmail.com>
2020-06-14 01:07:38 +0200

    Rubocop pass

--- dotfiles-cmd/share/dotf/file_rsync.rb
+++ dotfiles-cmd/share/dotf/file_rsync.rb
@@ -1,28 +1,25 @@
+# frozen_string_literal: true
+
 class FileRsync
+  @quiet = false
+  @dry_run = false
 
-    @quiet = false
-    @dry_run = false
+  def initialize(dry_run: false, quiet: false)
+    @dry_run = dry_run
+    @quiet = quiet
 
-    def initialize dry_run: false, quiet: false
+    @rsync_args =  '-av'
+    @rsync_args += ' --dry-run' if @dry_run
+    @rsync_args += ' -q' if @quiet
+  end
 
-        @dry_run = dry_run
-        @quiet = quiet
+  def rsync_files(source_path, dest_path)
+    cmd = "rsync #{@rsync_args} #{source_path} #{dest_path}"
 
-        @rsync_args =  "-av" 
-        @rsync_args += " --dry-run" if @dry_run
-        @rsync_args += " -q" if @quiet
-    end
-
-    def rsync_files source_path, dest_path 
-        cmd = "rsync #{@rsync_args} #{source_path} #{dest_path}"
-
-        dry_run_warning = ""
-        dry_run_warning = "(DRY RUN)" if @dry_run
-        puts "#{dry_run_warning} #{source_path} --> #{dest_path}"
-        unless @quiet
-            puts "executing command: [#{cmd}]"
-        end
-        `#{cmd}`
-    end
-
+    dry_run_warning = ''
+    dry_run_warning = '(DRY RUN)' if @dry_run
+    puts "#{dry_run_warning} #{source_path} --> #{dest_path}"
+    puts "executing command: [#{cmd}]" unless @quiet
+    `#{cmd}`
+  end
 end
--- dotfiles-cmd/share/dotf/homebrew.rb
+++ dotfiles-cmd/share/dotf/homebrew.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'optparse'
 require 'mkmf'
 require 'open3'
@@ -5,114 +7,111 @@ require 'open3'
 MakeMakefile::Logging.instance_variable_set(:@logfile, File::NULL)
 
 def install_homebrew
-	app_name = "Homebrew"
-	app_cmd = "brew"
-	app_install_cmd = 'ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"'
+  app_name = 'Homebrew'
+  app_cmd = 'brew'
+  app_install_cmd = 'ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"'
 
-	brewfile_filepath = "#{ENV["_DOTF_ROOT"]}/../configuration/Brewfile"
+  brewfile_filepath = "#{ENV['_DOTF_ROOT']}/../configuration/Brewfile"
 
-	puts "---------------------------------------------------------------------------------"
-	puts "This script installs #{app_name} if it is not installed"
-	puts "---------------------------------------------------------------------------------"
+  puts '---------------------------------------------------------------------------------'
+  puts "This script installs #{app_name} if it is not installed"
+  puts '---------------------------------------------------------------------------------'
 
-	puts 'Looking for Homebrew executable...'
-	if find_executable(app_cmd).nil?
+  puts 'Looking for Homebrew executable...'
+  if find_executable(app_cmd).nil?
 
-	puts "#{app_name} was not found in the system, To install it, this command needs be executed:"
-	puts "#{app_install_cmd}"
+    puts "#{app_name} was not found in the system, To install it, this command needs be executed:"
+    puts app_install_cmd.to_s
 
-	puts "Do you want me to install it? (y/n)"
+    puts 'Do you want me to install it? (y/n)'
 
-	if gets.chomp =~ /^[Yy]/
-		puts "Ok, installing #{app_name}"
-		system app_install_cmd
-	else
-		puts "Aborting"
-		exit 2
-	end
+    if gets.chomp =~ /^[Yy]/
+      puts "Ok, installing #{app_name}"
+      system app_install_cmd
+    else
+      puts 'Aborting'
+      exit 2
+    end
 
-	else
-	puts "Found #{app_name} in the system: version #{%x(#{app_cmd} --version)}"
-	end
+  else
+    puts "Found #{app_name} in the system: version #{`#{app_cmd} --version`}"
+  end
 end
 
+def bundle_homebrew(brewfile_path)
+  unless File.exist? File.expand_path brewfile_path
+    puts "Couldn't find a Brewfile at path '#{brewfile_path}'"
+    exit 1
+  end
 
-def bundle_homebrew brewfile_path
+  options = {}
+  OptionParser.new do |opt|
+    opt.on('--dry-run') { |o| options[:dry_run] = o }
+    opt.on('--verbose') { |o| options[:verbose] = o }
+    opt.on('--taps') { |o| options[:taps] = o }
+    opt.on('--casks') { |o| options[:casks] = o }
+    opt.on('--mas') { |o| options[:mas] = o }
+  end.parse!
+  install_homebrew
 
-	unless File.exist? File.expand_path brewfile_path
-		puts "Couldn't find a Brewfile at path '#{brewfile_path}'"
-		exit 1
-	end
+  fine_grain_modifiers = %i[taps brews casks mas]
+  supported_options = %i[verbose dry_run] + fine_grain_modifiers
 
-	options = {}
-	OptionParser.new do |opt|
-		opt.on('--dry-run') { |o| options[:dry_run] = o }
-		opt.on('--verbose') { |o| options[:verbose] = o }
-		opt.on('--taps') { |o| options[:taps] = o }
-		opt.on('--casks') { |o| options[:casks] = o }
-		opt.on('--mas') { |o| options[:mas] = o }
-	end.parse!
-	install_homebrew()
+  # check for invalid parameters
+  invalid_options = options.slice(supported_options)
 
-	fine_grain_modifiers = [:taps, :brews, :casks, :mas]
-	supported_options = [:verbose, :dry_run] + fine_grain_modifiers
+  if options[:verbose] == true
+    puts 'Arguments passed:'
+    options.each do |kv|
+      puts "\t#{kv[0]} => #{kv[1]}"
+    end
+  end
 
-	# check for invalid parameters
-	invalid_options = options.slice(supported_options)
+  unless invalid_options.empty?
+    puts "Unrecogniced arguments(s): #{invalid_options}"
+    exit 3
+  end
 
-	if (options[:verbose] == true)
-		puts "Arguments passed:"
-		options.each do |kv|
-			puts "\t#{kv[0]} => #{kv[1]}"
-		end
-	end
+  #  if dry-run is active we will just use the bundle list command to print
+  #  the actions to be performed
+  base_command = "brew bundle install #{options[:verbose] ? '--verbose' : ''} --file \"#{brewfile_path}\""
 
-	unless invalid_options.empty?
-		puts "Unrecogniced arguments(s): #{invalid_options}"
-		exit 3
-	end
+  commands_to_execute = ['brew update']
 
-	# if dry-run is active we will just use the bundle list command to print
-	# the actions to be performed
-	base_command = "brew bundle install #{options[:verbose]? "--verbose" : ""} --file \"#{brewfile_path}\""
+  modifiers = options.slice(*fine_grain_modifiers)
+  if modifiers.empty?
+    commands_to_execute << base_command
+  else
+    modifiers.each_key do |modifier|
+      commands_to_execute << "#{base_command} --#{modifier}"
+    end
+  end
 
-	commands_to_execute = ['brew update']
+  commands_to_execute << 'brew cleanup'
 
-	modifiers = options.slice(*fine_grain_modifiers)
-	if (modifiers.empty?)
-		commands_to_execute << base_command
-	else
-		modifiers.each_key do |modifier|
-			commands_to_execute << "#{base_command} --#{modifier}"
-		end
-	end
+  if options[:dry_run]
+    puts 'Running in dry-run mode. This script will perform the following commands:'
+    commands_to_execute.each do |cmd|
+      puts "\t* #{cmd}"
+    end
+    exit 0
+  else
+    commands_to_execute.each do |cmd|
+      puts
+      puts "Executing command: '#{cmd}'" if options[:verbose]
 
-	commands_to_execute << 'brew cleanup'
+      Open3.popen2e(cmd) do |_stdin, stdout_stderr, wait_thread|
+        Thread.new do
+          stdout_stderr.each { |line| puts line }
+        end
 
-	if options[:dry_run]
-		puts "Running in dry-run mode. This script will perform the following commands:"
-		commands_to_execute.each do |cmd|
-			puts "\t* #{cmd}"
-		end
-		exit 0
-	else
-		commands_to_execute.each  do |cmd|
-			puts
-			puts "Executing command: '#{cmd}'" if (options[:verbose])
+        status = wait_thread.value
+        exit status.exitstatus if status.exitstatus != 0
+      end
+    end
+  end
 
-			Open3.popen2e(cmd) do |stdin, stdout_stderr, wait_thread|
-				Thread.new do
-					stdout_stderr.each {|line| puts line }
-				end
-
-				status = wait_thread.value
-				exit status.exitstatus if status.exitstatus != 0
-			end
-		end
-	end
-
-	puts
-	puts "Done!"
-	puts "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-
+  puts
+  puts 'Done!'
+  puts "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
 end
