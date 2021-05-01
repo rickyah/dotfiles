76752e9657a1e64558b98c904c9f94c668ece3f3
Ricardo Amores <ricardo.amores@scopely.com>
2021-05-01 12:39:13 +0200

    Use powerlevel10k zshell theme

--- configuration/config-files/shell/zshrc.sh
+++ configuration/config-files/shell/zshrc.sh
@@ -8,7 +8,8 @@ ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="agnoster"
+# ZSH_THEME="agnoster"
+source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
 DEFAULT_USER=ricky
 
 # Example aliases
