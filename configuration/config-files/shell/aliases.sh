0825fbf048205ca54e7149555155e06d2b43b899
Ricardo Amores <ricardo.amores@scopely.com>
2021-05-04 18:14:46 +0200

    Use exa instead of ls

--- configuration/config-files/shell/aliases.sh
+++ configuration/config-files/shell/aliases.sh
@@ -16,6 +16,9 @@
 # find aliases
 alias fn='find . -iname $1'
 
+# exa (replacement for ls)
+alias ls=exa
+
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update'
 
