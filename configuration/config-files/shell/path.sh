d063ae7e311d6f788d6ee0ae5e0dc8d9951ba7f5
Ricardo Amores <rickyah@gmail.com>
2022-08-16 09:00:15 +0200

    fix(shell): correctly set homebrew path

--- configuration/config-files/shell/path.sh
+++ configuration/config-files/shell/path.sh
@@ -1,6 +1,9 @@
 # default path
 PATH=PATH:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 
+# Homebrew path
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # my custom paths
 CURRENT_DIR_BIN_PATH=$PATH:$HOME/bin:./bin
 DOTNET_CORE_PATH=/usr/local/share/dotnet
@@ -8,13 +11,6 @@ MONO_PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin
 GNU_CORE_UTILS_PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 
 PATH=$PATH:$CURRENT_DIR_BIN_PATH:$GNU_CORE_UTILS_PATH:$DOTNET_CORE_PATH:$MONO_PATH
+
 export MONO_PATH
-
-# Homebrew exports (these exports come from running 'brew shellenv'
-export HOMEBREW_PREFIX="/usr/local";
-export HOMEBREW_CELLAR="/usr/local/Cellar";
-export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
-export PATH="/usr/local/bin:/usr/local/sbin${PATH+:$PATH}";
-export MANPATH="/usr/local/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/usr/local/share/info:${INFOPATH:-}";
-
+export PATH
